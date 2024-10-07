825bdcbaa5e53b1750730a2a9a83ac8c134bbe34
LinderJK <slepchenkovia@gmail.com>
2024-10-08 00:15:55 +0300

    feat: add check username and create readline

---
+++ index.js
@@ -0,0 +1,50 @@
+import { createInterface } from "readline";
+
+const fileManager = () => {
+  const args = process.argv.slice(2);
+  const userName = args.find((arg) => arg.startsWith('--username=')).split('=')[1];
+
+  if(!userName) {
+    console.log('Please provide a username');
+    return;
+  } else {
+    console.log(`Welcome to the File Manager, Username!, ${userName}`);
+  }
+
+  const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  rl.question('Please enter the command, you can use "--help" for more information\n', (command) => {
+    switch(command) {
+      case '--help':
+        console.log(`
+        help - help
+        list - list files
+        create - create file
+        delete - delete file
+        `);
+        break;
+      case 'list':
+        console.log('Listing files...');
+        break;
+      case 'create':
+        console.log('Creating file...');
+        break;
+      case 'delete':
+        console.log('Deleting file...');
+        break;
+      default:
+        console.log('Invalid command');
+        break;
+    }
+    rl.on('close', () => {
+      console.log(`Thank you for using File Manager, ${userName}, goodbye!`);
+      process.exit(0);
+      }
+    )
+  });
+}
+
+fileManager();
