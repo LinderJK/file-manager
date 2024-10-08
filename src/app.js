da798b88c9f22fd999fef9b2f02a32b9d2b9d1ad
LinderJK <slepchenkovia@gmail.com>
2024-10-08 08:28:27 +0000

    refactor: add app js and move logic intro file, change index to entry point

---
+++ src/app.js
@@ -0,0 +1,81 @@
+import { createInterface } from "readline";
+import { commands } from "./helpers/commands.js";
+import { userName } from "./helpers/constants.js";
+import os from "os";
+import goUp from "./commands/up.js";
+
+
+const fileManager = () => {
+  let currentDirectory = os.homedir();
+  if(currentDirectory) {
+    process.chdir(currentDirectory);
+  }
+  if (!userName) {
+    console.log("Please provide a username");
+    return;
+  } else {
+    console.log(`Welcome to the File Manager, ${userName}!`);
+    console.log("Enter the command. Type 'help' for a list of commands.");
+    console.log(`You are currently in '${currentDirectory}'`);
+    
+  }
+
+  const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  rl.on("line", async(line) => {
+    const [command, ...args] = line.trim().split(" ");
+    try {
+      switch (command) {
+        case "help":
+          console.table(commands);
+          break;
+        // case "cd":
+        //   await cd(args);
+        //   break;
+        case "up":
+          await goUp();
+          break;
+        // case "ls":
+        //   await ls();
+        //   break;
+        // case "add":
+        //   await add(args);
+        //   break;
+        // case "cat":
+        //   await cat(args);
+        //   break;
+        // case "hash":
+        //   await hash(args);
+        //   break;
+        // case "compress":
+        //   await compress(args);
+        //   break;
+        // case "decompress":
+        //   await decompress(args);
+        //   break;
+        case '.exit':
+          rl.emit('SIGINT');
+          break;
+        default:
+          console.log("Invalid command");
+          break;
+      }
+        currentDirectory = process.cwd();
+       console.log(`You are currently in ${currentDirectory}`);
+    } catch (error) {
+      console.error(error);
+    }
+  });
+
+  rl.on('SIGINT', () => {
+    console.log(`Thank you for using File Manager, ${userName}, goodbye!`);
+    process.exit();
+  })
+
+
+};
+
+fileManager();
