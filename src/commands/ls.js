2ff33edd575e53baec95a50f9b332ee56ac365d6
LinderJK <slepchenkovia@gmail.com>
2024-10-08 11:21:55 +0000

    feat: add lstat for files in dir and sort results

--- src/commands/ls.js
+++ src/commands/ls.js
@@ -1,12 +1,40 @@
 import fs from "fs/promises";
-export default async function ls(args) {
+import path from "path";
+export default async function ls(args, useTab = false) {
   if (args.length > 0) {
     console.log("Invalid input: command requires no arguments.");
     return;
   }
   const currentDir = process.cwd();
   const files = await fs.readdir(currentDir);
-  console.log(files[0], 'debug!!!');
+  const result = await Promise.all(
+    files.map(async (file) => {
+      const itemPath = path.join(currentDir, file);
+      const stats = await fs.lstat(itemPath);
+      
+      return {
+        name: file,
+        path: itemPath,
+        type: stats.isDirectory() ? "directory" : "file",
+        ext: path.extname(file),
+      }
+    })
+  )
 
-  console.log(files.join(" "));
+  result.sort((a, b) => {
+    if (a.type === "directory" && b.type !== "directory") {
+      return -1;
+    } else if (a.type !== "directory" && b.type === "directory") {
+      return 1;
+    } else {
+      return a.name.localeCompare(b.name);
+    }
+  });
+  
+
+  if (useTab) {
+    console.log(files.join("\t"));
+  }
+
+  console.table(result);
 }
