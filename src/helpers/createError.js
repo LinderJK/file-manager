867d4fcf067b38dd11eba8ad57454e2c9fe2e4da
LinderJK <slepchenkovia@gmail.com>
2024-10-08 14:55:34 +0000

    refactor: change errors text

--- src/helpers/createError.js
+++ src/helpers/createError.js
@@ -4,9 +4,9 @@ export default function createError (error) {
   
   switch (error.code) {
     case "ENOENT":
-      throw new Error("Operation failed: The file/directory does not exist.");
+      throw new Error("Invalid input: The file/directory does not exist.");
     case "EACCES":
-      throw new Error("Operation failed: The file is not readable.");
+      throw new Error("Invalid input: The file is not readable.");
     case "ERR_INVALID_ARG_TYPE":
       throw new Error("Invalid input: wrong argument type.");
     default:
