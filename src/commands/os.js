789419da1552044e476a3a806fea4b09f832ca49
LinderJK <slepchenkovia@gmail.com>
2024-10-08 21:49:00 +0300

    feat: add homedir, username, architecture

--- src/commands/os.js
+++ src/commands/os.js
@@ -1,6 +1,5 @@
 import os from "os";
 
-
 export default async function osInfo(args) {
   if (args.length !== 1) {
     console.log("Invalid input: wrong argument type.");
@@ -11,13 +10,27 @@ export default async function osInfo(args) {
   console.log(flag);
   switch (flag) {
     case "--EOL":
-      process.stdout.write(`End of Line (EOL) marker: ${JSON.stringify(os.EOL)} \n`);
+      process.stdout.write(
+        `End of Line (EOL) marker: ${JSON.stringify(os.EOL)} \n`,
+      );
       break;
-      case "--cpus":
-        getCpus();
-        break;
-
-}
+    case "--cpus":
+      getCpus();
+      break;
+    case "--homedir":
+      process.stdout.write(
+        `Home directory: ${JSON.stringify(os.homedir())} \n`,
+      );
+      break;
+    case "--username":
+      process.stdout.write(
+        `Username: ${JSON.stringify(os.userInfo().username)} \n`,
+      );
+      break;
+    case "--architecture":
+      process.stdout.write(`Architecture: ${JSON.stringify(os.arch())} \n`);
+      break;
+  }
 }
 
 function getCpus() {
@@ -25,7 +38,7 @@ function getCpus() {
   console.log(`Total CPUs: ${cpus.length}`);
   console.log(`Model: ${cpus[0].model}`);
   cpus.forEach((cpu, index) => {
-    console.log(`Speed: ${(index + 1)} - ${(cpu.speed / 1000).toFixed(2)} GHz`);
-    console.log('------------');
-  })
+    console.log(`Speed: ${index + 1} - ${(cpu.speed / 1000).toFixed(2)} GHz`);
+    console.log("------------");
+  });
 }
